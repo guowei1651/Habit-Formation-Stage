652e9d0a50e5344062a8f709df187ff29b9ce6f0
wales kuo <guowei1651@gmail.com>
2025-01-09 15:54:34 +0000

    device page modified

--- pages/devices/jsobjects/devicesView/devicesView.js
+++ pages/devices/jsobjects/devicesView/devicesView.js
@@ -1,14 +1,39 @@
 export default {
 	DeviceTypes:[],
-	DeviceTypesMap: {},
 	onload () {
 		findAllDeviceTypes.run()
 
 		var self = this
 		findAllDeviceTypes.data.map(deviceType => {
-			self.DeviceTypes[deviceType.id.toString()] = deviceType.name
+			self.DeviceTypes[deviceType.id.toString()] = {
+				'name':deviceType.name, 
+				'allow_deletion': deviceType.allow_deletion
+			}
 		})
-		
+
 		console.log("最终结果", JSON.stringify(self.DeviceTypes))
+	},
+	showChageDeviceModal () {
+		var data = devicesTable.selectedRow
+		chageSerialInput.setValue(data.serial)
+		chageNameInput.setValue(data.name)
+		chageTypeInput.setValue(data.type_id)
+		chageDescInput.setValue(data.info)
+		chageFirmwareVerInput.setValue(data.firmware_version)
+		showModal(chageDeviceModal.name)
+	},
+	actionAddDeviceModalSubmit () {
+		var data = addSerialInput.text
+		const param = {
+			'serial': data
+		}
+		insertMyDevice.run(param).then(()=>{
+			findMyDevices.run()
+			closeModal(addDeviceModal.name)
+		}, (err) => {
+			showAlert("发生错误，请稍后重试。"+JSON.stringify(err), 'error')
+		}).catch((err) => {
+			showAlert("发生错误，请稍后重试。"+JSON.stringify(err), 'error')
+		})
 	}
 }
