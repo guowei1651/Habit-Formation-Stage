08fb3dba3c8dc1c069e973605fb27437c3a8da63
wales kuo <guowei1651@gmail.com>
2025-01-25 09:54:55 +0000

    提交

---
+++ pages/carousel_item/jsobjects/carouselsView/carouselsView.js
@@ -0,0 +1,100 @@
+export default {
+	carouselItems: [],
+	carouselItemSlider: [],
+	habit: [],
+	currentItem: {
+		index: 0,
+		setIndex(i) {
+			this.currentItem.index = i
+		},
+		getItem() {
+			let self = this.currentItem
+			console.log("CurrentItem getItem ", this)
+			if (!this.carouselItems || this.carouselItems.length == 0) {
+				return "remind"
+			}
+			let itemCount = self.gloabThis.carouselItems.length
+			if (self.index < 0 || self.index >= itemCount) {
+				return "remind"
+			}
+			return self.gloabThis.carouselItems[this.index]
+		},
+		getDisplayTypeValue() {
+			return this.getItem().displayType
+		},
+		getPageTypeValue() {
+			return this.getItem().pageType
+		},
+		showCarouselItem () {
+			var item = this.carouselItems[this.index]
+
+			AlertLevelSelect.setSelectedOption(item.alertLevel)
+			DurationInput.setValue(item.duration)
+			TriggerTimeInput.setValue(item.triggerTime)
+			Image.setImage(item.chartUrl)
+			HabitSelect.setSelectedOption(item.habitId)
+		}
+	},
+	async onload () {
+		console.log("carousel_item onload", appsmith.store.growing)
+		if (! appsmith.store.growing || ! appsmith.store.growing.id) {
+			navigateTo('login')
+			return
+		}
+
+		console.log("carousel_item onload", appsmith.URL.queryParams)
+		if (!appsmith.URL.queryParams) {
+			console.log("未传参数，所以不能正常使用。")
+			showAlert("未传参数，所以不能正常使用。")
+			return;
+		}
+
+		this.carouselItems = []
+
+		await getHabit.run();
+		this.habit = getHabit.data
+
+		CategorySlider.setValue(0)
+		this.currentItem.setIndex(index)
+		console.log(this.currentItem)
+		this.currentItem.showCarouselItem()
+	},
+	async fillCarouselItemSlider() {
+		var result = []
+		for (var i = 1 ;i < this.carouselItems.length + 1; i++)
+		{ 
+			result.push({
+				"label": i + "",
+				"value": i
+			})
+		}
+		this.carouselItemSlider = result
+		return result
+	},
+	async addItem () {
+		console.log("addItem clider value: ",CategorySlider.value)
+		var item = {
+			displayType: "remind",
+			pageType: "habit",
+			alertLevel: '',
+			duration: 0,
+			triggerTime: '',
+			chartUrl: '',
+			habitId: 0,
+		}
+		this.carouselItems.splice(CategorySlider.value - 1, 0, item);
+
+		await this.fillCarouselItemSlider()
+	},
+	async removeItem () {
+		console.log("removeItem slider value: ",CategorySlider.value)
+		if (CategorySlider.value >= this.carouselItems.length){
+			CategorySlider.setValue(CategorySlider.value - 1)
+		}
+		this.carouselItems.splice(CategorySlider.value - 1, 1)
+
+		await this.fillCarouselItemSlider()
+	},
+}
+
+
--- pages/home/jsobjects/homeView/homeView.js
+++ pages/home/jsobjects/homeView/homeView.js
@@ -6,8 +6,57 @@ export default {
 			navigateTo('login')
 		}
 	},
-	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
+	async showForm(isCreate, data) {
+		console.log("showForm ", isCreate, data)
+		promptText.setText("")
+		if (isCreate) {
+			titleText.setText("创建")
+			idInput.setValue("")
+			nameInput.setValue("")
+			descInput.setValue("")
+			Form.data['id'] = undefined
+		} else {
+			titleText.setText("修改")
+			idInput.setValue(data.id)
+			nameInput.setValue(data.name)
+			if(data.description) {
+				descInput.setValue(data.description)
+			} else {
+				descInput.setValue("")
+			}
+		}
+		showModal("detail")
+	},
+	async createOrUpdate () {
+		console.log("createOrUpdate", Form.data)
+		await existCarousel.run({"name":Form.data['nameInput']})
+		console.log(existCarousel.data)
+		if (existCarousel.data[0].count > 0) {
+			promptText.setText("*已经存在名字相同的轮播！")
+			return
+		}
+
+		if (!Form.data.idInput) {
+			let data = {}
+			data['name'] = Form.data['nameInput']
+			data['description'] = Form.data['descInput']
+			createCarousel.run(data).then(()=>{
+				getCarousel.run()
+				closeModal("detail")
+			}).catch((err)=>{
+				showAlert("创建失败，"+err.message, "error")
+			})
+		} else {
+			let data = {}
+			data['id'] = Form.data['idInput']
+			data['name'] = Form.data['nameInput']
+			data['description'] = Form.data['descInput']
+			updateCarousel.run(data).then(()=>{
+				getCarousel.run()
+				closeModal("detail")
+			}).catch((err)=>{
+				showAlert("创建失败，"+err.message, "error")
+			})
+		}
 	}
 }
